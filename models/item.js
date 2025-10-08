23b1986e5def46014903510f6ce545e20251c450
ecmdigitalai-wq <ecmdigitalai@gmail.com>
2025-10-08 14:15:24 +0530

    Update item.js

--- models/item.js
+++ models/item.js
@@ -1,1 +1,10 @@
+import mongoose from "mongoose";
 
+const itemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  link: { type: String, required: true },
+  image: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Item", itemSchema);
